2c38d4f97c11a67d2cc7cc2014c5741f2e82701e
Joshua Liu <mathmanjosh@gmail.com>
2024-04-09 12:48:36 +0000

    fix build (big cook)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Suspense
+          fallback={
+            <div className="flex justify-center items-center select-none h-screen bg-black text-white font-mono font-bold p-10 relative">
+              <span>Loading...</span>
+            </div>
+          }
+        >
+          {children}
+        </Suspense>
+      </body>
     </html>
   );
 }
--- src/app/signup/actions.ts
+++ src/app/signup/actions.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import { User } from "@haelp/auth";
-import { auth , checkValidUsername} from "@lib";
+import { auth, checkValidUsername } from "@lib";
+import { checkValidEmail } from "@lib/email";
 import { APIResponse } from "@types";
 import { cookies } from "next/headers";
 
@@ -9,6 +10,9 @@ export const signup = async (
   user: Omit<User, "id"> & { password: string }
 ): Promise<APIResponse<User>> => {
   try {
+    const validationRes = await validateUser(user);
+    if (validationRes !== true) return { success: false, error: validationRes };
+
     const { user: resUser, jwt } = await auth.createUser(user);
 
     cookies().set(auth.jwt.cookie, jwt, {
@@ -27,8 +31,14 @@ export const checkIdentifier = async (email: string): Promise<boolean> => {
   return await auth.checkIdentifier(email);
 };
 
-export const validateUser =async (user: User) => {
+export const validateUser = async (user: Omit<User, "id">) => {
   // validate username
-	if (!checkValidUsername(user.username) || await auth.checkIdentifier(user.username)) return false; 
-	
+  if (!checkValidUsername(user.username)) return "Invalid username";
+  if (await auth.checkIdentifier(user.username)) return "Username taken";
+
+  // validate email
+  if (!checkValidEmail(user.email)) return "Invalid email";
+  if (await auth.checkIdentifier(user.email)) return "Email already in use";
+
+  return true;
 };
